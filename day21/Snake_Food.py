9eab465ea7345fdcc75f9514e9034ec0cef1dd60
ABBURI VENKATA RAVI TEJA <118928330+tejabburi3@users.noreply.github.com>
2026-01-23 22:35:30 +0530

    Snake_Food.py

---
+++ day21/Snake_Food.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+import random
+
+class Food(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("blue")
+        self.penup()
+        self.shapesize(0.7, 0.7)
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        self.goto(x, y)
